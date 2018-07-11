d5d9f8bd33a106775e056c2296308e096538ab04
elliott <elliottcgriffin@gmail.com>
2018-07-11 13:10:55 -0400

    Convert to MVC design pattern.

---
+++ Todo List/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todo List
+//
+//  Created by elliott on 7/10/18.
+//  Copyright © 2018 Elliott Griffin. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
